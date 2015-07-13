ada0ab030f5bfcd850867f6e54f7d4b450504d5b
jleahred <jleahred@gmail.com>
2015-07-14 00:24:34 +0200

    rotation in a separated file

--- langs/rust/rubiks_solver/src/main.rs
+++ langs/rust/rubiks_solver/src/main.rs
@@ -16,53 +16,65 @@ fn main() {
 
     println!("\n\n{}", cube);
 
+
+    {
+        use cube::rotation;
+        use cube::rotation::{Orientation, Direction};
+
+        let rotation1 = rotation::process(&cube, &rotation::Item(Orientation::Horizontal, Direction::Plus, 0));
+        let rotation2 = rotation::process(&rotation1, &rotation::Item(Orientation::Vertical, Direction::Minus, 2));
+        println!("{}", rotation2);
+    }
+
+/*
     {
         use cube::rotation::*;
 
         let mv = cube::rotation::Item(Orientation::Horizontal, Direction::Plus, 0);
         println!("{}", mv);
 
-        let rotationd1 = cube::rotation_horizontal(&cube, Direction::Plus, 0);
+        let rotationd1 = cube::rotation::horizontal(&cube, Direction::Plus, 0);
         println!("{}", rotationd1);
 
-        //let rotationd2 = cube::rotation_horizontal(&rotationd1, Direction(false), 0);
+        //let rotationd2 = cube::rotation::horizontal(&rotationd1, Direction(false), 0);
         //println!("{}", rotationd2);
-        let rotationd2 = cube::rotation_horizontal(&rotationd1, Direction::Plus, 1);
+        let rotationd2 = cube::rotation::horizontal(&rotationd1, Direction::Plus, 1);
         println!("{}", rotationd2);
 
-        let rotationd3 = cube::rotation_vertical(&cube, Direction::Plus, 1);
+        let rotationd3 = cube::rotation::vertical(&cube, Direction::Plus, 1);
         println!("{}", rotationd3);
 
-        let rotationd4 = cube::rotation_horizontal(&rotationd3, Direction::Plus, 1);
+        let rotationd4 = cube::rotation::horizontal(&rotationd3, Direction::Plus, 1);
         println!("{}", rotationd4);
     }
 
     {
         use cube::rotation::*;
 
-        let rotation1 = cube::rotation_vertical(&cube, Direction::Plus, 0);
+        let rotation1 = cube::rotation::vertical(&cube, Direction::Plus, 0);
         println!("{}", rotation1);
 
-        let rotation2 = cube::rotation_horizontal(&rotation1, Direction::Plus, 0);
+        let rotation2 = cube::rotation::horizontal(&rotation1, Direction::Plus, 0);
         println!("{}", rotation2);
     }
     {
         use cube::rotation::*;
 
-        let rotation1 = cube::rotation_front(&cube, Direction::Plus, 0);
+        let rotation1 = cube::rotation::front(&cube, Direction::Plus, 0);
         println!("{}", rotation1);
     }
     {
         use cube::rotation::*;
 
-        let rotation1 = cube::rotation_front(&cube, Direction::Minus, 2);
+        let rotation1 = cube::rotation::front(&cube, Direction::Minus, 2);
         println!("{}", rotation1);
     }
     {
         use cube::rotation::*;
 
-        let rotation1 = cube::rotation_vertical(&cube, Direction::Plus, 0);
-        let rotation2 = cube::rotation_front(&rotation1, Direction::Plus, 0);
+        let rotation1 = cube::rotation::vertical(&cube, Direction::Plus, 0);
+        let rotation2 = cube::rotation::front(&rotation1, Direction::Plus, 0);
         println!("{}", rotation2);
     }
+    */
 }
