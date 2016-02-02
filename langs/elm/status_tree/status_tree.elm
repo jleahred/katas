2e9b689fb69b4ceb2cc47485d6db18796393c23d
jleahred <jleahred@gmail.com>
2016-02-02 21:15:18 +0100

    done!!!

--- langs/elm/status_tree/status_tree.elm
+++ langs/elm/status_tree/status_tree.elm
@@ -5,7 +5,7 @@ import Http
 import Effects exposing (Effects, Never)
 import Task exposing (Task, andThen)
 import StartApp as StartApp
-import Graphics.Element exposing (show)
+--import Graphics.Element exposing (show)   --debug
 import Time
 
 import Json.Decode as Json exposing ((:=))
@@ -23,9 +23,9 @@ type Model
 
 
 type alias  Status =
-    { statusTree    : List NodeInfo
-    , expandedItems : List String
-    }
+  { statusTree    : List NodeInfo
+  , expandedItems : List String
+  }
 
 type NodeInfo =
   NodeInfo
@@ -41,7 +41,9 @@ type NodeStatus = OK | WARNING | ERROR
 
 initModel: Model
 initModel =
-    ModelMessage  "Initializing..."
+  ModelMessage  "Initializing..."
+
+
 
 
 --  MAIN
@@ -54,11 +56,11 @@ main =
 app: StartApp.App Model
 app =
   StartApp.start
-    { init = (init, Effects.none)
-    , update = update
-    , view = view
-    , inputs = [Signal.map JsonLoaded readingsMailbox2.signal]
-    }
+  { init = (init, Effects.none)
+  , update = update
+  , view = view
+  , inputs = [Signal.map JsonLoaded readingsMailbox2.signal]
+  }
 
 
 
@@ -70,29 +72,16 @@ init = initModel
 -- UPDATE
 
 type Action
-    = ToggleSection String
-    | JsonLoaded    Model
+  = ToggleSection String
+  | JsonLoaded    Model
 
 
-
-getExpList model =
-    case model of
-        ModelMessage _      -> []
-        ModelLoaded status  -> status.expandedItems
-
-copyExpaned2Model nwModel prevModel =
-    case nwModel of
-        ModelMessage _      -> nwModel
-        ModelLoaded status  ->
-                      ModelLoaded
-                      { status
-                      | expandedItems = getExpList prevModel}
 
 update: Action -> Model -> (Model, Effects Action)
 update action model =
-    case action of
-        JsonLoaded    nwModel -> (copyExpaned2Model nwModel model, Effects.none)
-        ToggleSection id      -> (toggleId model id, Effects.none)
+  case action of
+    JsonLoaded    nwModel -> (copyExpaned2Model nwModel model,  Effects.none)
+    ToggleSection id      -> (toggleId model id,                Effects.none)
 
 
 
@@ -102,31 +91,34 @@ update action model =
 
 view : Signal.Address Action -> Model -> Html
 view address model =
-    Html.body [ style [fontStyle] ]
+  Html.body
+    [ style [fontStyle] ]
     [ case model of
-          ModelLoaded  status  ->
-              Html.div []
-              [ listNodesToHtml address status.expandedItems status.statusTree
-              --, Html.fromElement <| show model    --  debug
-              ]
-          ModelMessage error   ->
-              Html.text error
+      ModelLoaded  status  ->
+        Html.div []
+        [ listNodesToHtml address status.expandedItems status.statusTree
+        --, Html.fromElement <| show model    --  debug
+        ]
+      ModelMessage error   ->
+        Html.text error
     ]
+
 
 listNodesToHtml: Signal.Address Action -> List String -> List NodeInfo  -> Html
 listNodesToHtml address toggledIds treeList =
-    Html.ul[]
-          (List.map (nodeToHtml address toggledIds) treeList)
+  Html.ul[]
+    (List.map (nodeToHtml address toggledIds) treeList)
+
 
 nodeToHtml: Signal.Address Action -> List String -> NodeInfo ->  Html
 nodeToHtml address toggledIds (NodeInfo nodeInfo) =
-    if List.length nodeInfo.childs==0
-        ||  not (List.member nodeInfo.id toggledIds)
-        then
-        nodeInfoToHtml address nodeInfo
-    else
-        Html.span[] <| [nodeInfoToHtml address nodeInfo]
-            ++ [listNodesToHtml address toggledIds nodeInfo.childs]
+  if List.length nodeInfo.childs==0 || not (List.member nodeInfo.id toggledIds)
+  then
+    nodeInfoToHtml address nodeInfo
+  else
+    Html.span[]
+      <| [nodeInfoToHtml address nodeInfo]
+      ++ [listNodesToHtml address toggledIds nodeInfo.childs]
 
 
 
@@ -135,7 +127,7 @@ nodeToHtml address toggledIds (NodeInfo nodeInfo) =
 
 port tasks: Signal (Task Never ())
 port tasks =
-    app.tasks
+  app.tasks
 
 
 lazy: (() -> Json.Decoder a) -> Json.Decoder a
@@ -146,12 +138,13 @@ lazy thunk =
 
 decodeTree: Json.Decoder NodeInfo
 decodeTree =
-    Json.object4 --JsonNodeInfo
-      (\t s i c -> NodeInfo {text=t, status=s, id=i, childs=c})
-      ("text" := Json.string)
-      ("status" := Json.string |> stringToNodeStatus)
-      ("id" := Json.string)
-      ("childs" := Json.list (lazy (\_-> decodeTree)))
+  Json.object4 --JsonNodeInfo
+    (\t s i c -> NodeInfo {text=t, status=s, id=i, childs=c})
+    ("text" := Json.string)
+    ("status" := Json.string |> stringToNodeStatus)
+    ("id" := Json.string)
+    ("childs" := Json.list (lazy (\_-> decodeTree)))
+
 
 stringToNodeStatus: Json.Decoder String -> Json.Decoder NodeStatus
 stringToNodeStatus d =
@@ -165,23 +158,23 @@ stringToNodeStatus d =
 
 getStatusFromServer: Task a ()
 getStatusFromServer =
-    let
-        modelFromLTree lt =
-            ModelLoaded
-            { statusTree    = lt
-            , expandedItems = []
-            }
-        resquest =
-            Http.post (Json.list decodeTree) "status.json" Http.empty
-            --Http.get (Json.list decodeTree) "http://127.0.0.1:8000/status.json"
-            --Http.get (Json.list decodeTree) "http://100.100.16.64:8000/status.json"
-            |> Task.map modelFromLTree
-    in
-        resquest
-            `Task.onError`
-                (\err -> Task.succeed (ModelMessage <| toString err))
-            `Task.andThen`
-                (\m -> Signal.send readingsMailbox2.address m)
+  let
+    modelFromLTree lt =
+      ModelLoaded
+      { statusTree    = lt
+      , expandedItems = []
+      }
+    resquest =
+      Http.post (Json.list decodeTree) "status.json" Http.empty
+      --Http.get (Json.list decodeTree) "http://127.0.0.1:8000/status.json"
+      --Http.get (Json.list decodeTree) "http://100.100.16.64:8000/status.json"
+      |> Task.map modelFromLTree
+  in
+    resquest
+      `Task.onError`
+          (\err -> Task.succeed (ModelMessage <| toString err))
+      `Task.andThen`
+          (\m -> Signal.send readingsMailbox2.address m)
 
 port periodicTasks : Signal (Task () ())
 port periodicTasks = Signal.map (\_ -> getStatusFromServer)
@@ -199,6 +192,23 @@ readingsMailbox2 = Signal.mailbox initModel
 
 --  SUPPORT
 
+copyExpaned2Model: Model -> Model -> Model
+copyExpaned2Model nwModel prevModel =
+  case nwModel of
+    ModelMessage _      ->
+                  nwModel
+    ModelLoaded status  ->
+                  ModelLoaded
+                  { status
+                  | expandedItems = getExpList prevModel}
+
+getExpList: Model -> List String
+getExpList model =
+  case model of
+    ModelMessage _      -> []
+    ModelLoaded status  -> status.expandedItems
+
+
 nodeInfoToHtml: Signal.Address Action
                 -> { a | id : String
                    , text : String
@@ -207,69 +217,69 @@ nodeInfoToHtml: Signal.Address Action
                    }
                 -> Html
 nodeInfoToHtml address nodeInfo=
-    let
-        listStyle =
-            style [("list-style", "none")]
-        buttonStyle =
-            style [ ("border", "none")
-                  , ("background", "none")
-                  , fontStyle]
-        styleLI nChilds=
-            if nChilds == 0 then
-                [("color", "transparent")]
-            else
-                []
-        listIndicator ni =
-            Html.span [ style <| (styleLI <| List.length nodeInfo.childs)
-                              ++ [("margin-left", "7px")]
-                      ]
-                      [ text " · "]
-        statusColor status =
-            case status of
-              OK       -> "green"
-              WARNING  -> "orange"
-              _        -> "red"
-        circleStatus status =
-            Html.div [ style [ ("border-radius", "30px")
-                             , ("margin-top", "3px")
-                             , ("width", "15px")
-                             , ("height", "15px")
-                             , ("background", statusColor status)
-                             , ("color", "transparent")
-                             , ("float", "left")
-                             ]
-                      ]
-                      [ text "_"]
-
-    in
-        li [listStyle]
-           [ button [ onClick address (ToggleSection nodeInfo.id)
-                   , buttonStyle
-                   ]
-                   [ circleStatus  nodeInfo.status
-                   , listIndicator nodeInfo
-                   , text <| nodeInfo.text
-                   ]
-           ]
+  let
+    listStyle =
+      style [("list-style", "none")]
+    buttonStyle =
+      style [ ("border", "none")
+            , ("background", "none")
+            , fontStyle]
+    styleLI nChilds=
+      if nChilds == 0 then
+          [("color", "transparent")]
+      else
+          []
+    listIndicator ni =
+      Html.span [ style <| (styleLI <| List.length nodeInfo.childs)
+                        ++ [("margin-left", "7px")]
+                ]
+                [ text " · "]
+    statusColor status =
+      case status of
+        OK       -> "green"
+        WARNING  -> "orange"
+        _        -> "red"
+    circleStatus status =
+      Html.div [ style [ ("border-radius", "30px")
+                       , ("margin-top", "3px")
+                       , ("width", "15px")
+                       , ("height", "15px")
+                       , ("background", statusColor status)
+                       , ("color", "transparent")
+                       , ("float", "left")
+                       ]
+                ]
+                [ text "_"]
+  in
+    li [listStyle]
+     [ button [ onClick address (ToggleSection nodeInfo.id)
+             , buttonStyle
+             ]
+             [ circleStatus  nodeInfo.status
+             , listIndicator nodeInfo
+             , text <| nodeInfo.text
+             ]
+     ]
 
 
 toggleId: Model -> String -> Model
 toggleId model id =
-    case model of
-        ModelLoaded status ->
-            ModelLoaded
-            { status
-            | expandedItems
-                = if not (List.member id status.expandedItems) then
-                    status.expandedItems ++ [id]
-                  else
-                    List.filter (\i -> i/=id)  status.expandedItems
-            }
-        _ ->
-            ModelMessage "Error, received toggle with invalid status tree"
+  case model of
+    ModelLoaded status ->
+      ModelLoaded
+      { status
+      | expandedItems
+          = if not (List.member id status.expandedItems) then
+              status.expandedItems ++ [id]
+            else
+              List.filter (\i -> i/=id)  status.expandedItems
+      }
+    _ ->
+      ModelMessage "Error, received toggle with invalid status tree"
 
 
 
+fontStyle : (String, String)
 fontStyle = ("font-size", "1.1em")
 
 
