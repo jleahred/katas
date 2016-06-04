60d5975ddaf64951ee5df3c4d88e6b460ffbe6a7
maiquel <jleahred@gmail.com>
2016-06-04 19:46:28 +0200

    adding session receiver support

--- langs/elixir/elixfix/lib/f/fsessionreceiver.ex
+++ langs/elixir/elixfix/lib/f/fsessionreceiver.ex
@@ -1,7 +1,5 @@
 defmodule  FSessionReceiver  do
-  import FMsgMapSupport, only: [check_tag_value: 3,
-                                get_tag_value_mandatory_int: 2,
-                                check_mandatory_tags: 2]
+  import FMsgMapSupport, only: [check_tag_value: 3]
 
   defmodule Status do
     @doc """
@@ -32,7 +30,7 @@ Possible actions are:
 
     * :none
     * :reset_sequence
-    * reject_msg: description
+    * {:reject_msg, description}
   """
   def process_message(status, msg_map) do
       {_, errors} =
@@ -67,61 +65,12 @@ Possible actions are:
 
   defp logon(status, msg_map)  do
     case status.state do
-        :waitting_login   ->  process_logon(status, msg_map)
+        :waitting_login   ->  Support.process_logon(status, msg_map)
         :login_ok         ->
           {  %Status { status | state: :waitting_login },
               reject_msg: "received rq login on login status."
           }
 
-    end
-  end
-
-  defp process_logon(status, msg_map) do
-    {_, errors} =
-      { msg_map , []}
-      |>  check_mandatory_tags([96, 98, 108])
-      |>  check_tag_value(96, status.password)  # check password
-      |>  check_tag_value(98, 0)                # no encripytion
-
-
-    #errors = [FMsgParse.check_mandatory_tags(msg_map, [96, 98, 108])]
-
-    # check password
-    #errors = check_tag_value(96, status.password, msg_map, errors)
-    # no encription
-    #errors = check_tag_value(98, "0", msg_map, errors)
-    # reset sequence
-    { msg_seq, errors } =
-        case Map.get(msg_map, 141, nil)  do
-            "N"   ->   { status.msg_seq_num, errors }
-            "Y"   ->   { 0,                  errors }
-            nil ->     { status.msg_seq_num, errors }
-            other ->   { status.msg_seq_num, errors ++
-                ["Invalid value on tag 141 (ResetSeqNumFlag) rec: #{other}"] }
-        end
-
-    { hearbeat, errors } =
-        case get_tag_value_mandatory_int(108, msg_map)  do
-            { :ok,    val }   ->   { val, errors }
-            { :error, desc }  ->   { 0, errors ++
-                              ["Invalid value on tag 108 (Hearbeat) #{desc}"] }
-        end
-
-
-    if errors == []  do
-      {  %Status { status | state:            :login_ok,
-                            heartbeat_interv: hearbeat,
-                            msg_seq_num:      msg_seq},
-          if msg_seq == 0 do
-              :reset_sequence
-          else
-              :none
-          end
-      }
-    else
-      {  %Status { status | msg_seq_num:      msg_seq},
-          reject_msg: errors
-      }
     end
   end
 
