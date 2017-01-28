f4f1212d2e88bc84feeae2356f98b5dd6ccaf624
maiquel <jleahred@gmail.com>
2017-01-28 20:55:44 +0100

    init msg_parse

--- langs/rust/rustfix/src/msg_parse/mod.rs
+++ langs/rust/rustfix/src/msg_parse/mod.rs
@@ -6,23 +6,8 @@ use std::collections::BTreeMap;
 use std::collections::LinkedList;
 
 
-#[derive(Debug, PartialEq, Eq, Copy, Clone)]
-pub enum ParsingState {
-    ReadingTag,
-    ReadingValue,
-    Finished,
-}
-
-impl Default for ParsingState {
-    fn default() -> ParsingState {
-        ParsingState::ReadingTag
-    }
-}
-
-
-#[derive(Default, Debug, PartialEq, Eq, Clone)]
-pub struct Parsing {
-    pub parsing_state: ParsingState,
+#[derive(Debug)]
+pub struct Parsed {
     pub msg_map: BTreeMap<u32, String>,
     pub body_length: u32,
     pub check_sum: u32,
@@ -30,50 +15,74 @@ pub struct Parsing {
     pub orig_msg: String,
     pub errors: LinkedList<(u32, String)>,
     pub msg_length: u32,
-    pub curr_state_length: u32,
-    pub curr_tag_string: String,
-    pub curr_string: String,
 }
 
 
-impl Parsing {
-    pub fn add_char(&mut self, ch: char) -> () {
-        self.increase_counters(ch);
-        match self.parsing_state.clone() {
-            ParsingState::ReadingTag => self.add_char_reading_tag(ch),
-            ParsingState::ReadingValue => self.add_char_reading_value(ch),
-            ParsingState::Finished => self.add_char_reading_tag(ch),
-        };
-    }
-
-    fn increase_counters(&mut self, ch: char) -> () {
+impl Parsed {
+    fn add_char(mut self, ch: char) -> () {
         let repl_ch = if ch == 1u8 as char {
             '^'
         } else {
             ch
         };
         self.msg_length += 1;
-        self.curr_state_length += 1;
+        // self.curr_state_length += 1;
         self.orig_msg.push(repl_ch);
     }
+}
 
-    fn add_char_reading_tag(&mut self, ch: char) -> () {
-        if ch == '=' {
-            self.parsing_state = ParsingState::ReadingValue;
-            self.curr_state_length = 0;
-            self.curr_tag_string = self.curr_string.clone();
-        } else {
-            self.curr_string.push(ch);
+
+
+
+#[derive(Debug)]
+pub enum ParsingState {
+    StReadingTag {
+        tag: String,
+    },
+    StReadingValue {
+        tag: String,
+        value: String,
+    },
+    StFinished,
+}
+
+impl ParsingState {
+    pub fn add_char(self, ch: char) -> ParsingState {
+        match self {
+            ParsingState::StReadingTag { mut tag } => {
+                ParsingState::StReadingTag {
+                    tag: {
+                        tag.push(ch);
+                        tag
+                    },
+                }
+            }
+            ParsingState::StReadingValue { tag, mut value } => {
+                ParsingState::StReadingValue {
+                    tag: tag,
+                    value: {
+                        value.push(ch);
+                        value
+                    },
+                }
+            }
+            ParsingState::StFinished => ParsingState::StReadingTag { tag: String::new() },
         }
     }
+}
 
-    fn add_char_reading_value(&mut self, ch: char) -> () {
-        if ch == '\u{1}' {
-            self.parsing_state = ParsingState::Finished;
-            self.curr_state_length = 0;
-            self.curr_tag_string = String::new();
-        } else {
-            self.curr_string.push(ch);
-        }
+
+
+pub struct ParsingInfo {
+    parsing_state: ParsingState,
+    parsed: Parsed,
+}
+
+
+
+impl ParsingInfo {
+    pub fn add_char(self, ch: char) -> () {
+        self.parsed.add_char(ch);
+        self.parsing_state.add_char(ch);
     }
 }
--- langs/rust/rustfix/src/msg_parse/test.rs
+++ langs/rust/rustfix/src/msg_parse/test.rs
@@ -1,19 +1,19 @@
-use msg_parse::Parsing;
-use test_diff;
+// use msg_parse::Parsing;
+// use test_diff;
 
 
-#[test]
-fn run_tests() {
-    let mut finish = Parsing::default();
-    finish.add_char('\u{1}');
+// #[test]
+// fn run_tests() {
+//     let mut finish = Parsing::default();
+//     finish.add_char('\u{1}');
 
-    let check = Parsing {
-        orig_msg: "^".to_string(),
-        msg_length: 1,
-        curr_state_length: 1,
-        curr_string: "\u{1}".to_string(),
-        ..Parsing::default()
-    };
+//     let check = Parsing {
+//         orig_msg: "^".to_string(),
+//         msg_length: 1,
+//         curr_state_length: 1,
+//         curr_string: "\u{1}".to_string(),
+//         ..Parsing::default()
+//     };
 
-    assert_eq_dif!(finish, check);
-}
+//     assert_eq_dif!(finish, check);
+// }
