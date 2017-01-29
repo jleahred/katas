4954859bf44656338cef8448f1de4e562a2f09b1
maiquel <jleahred@gmail.com>
2017-01-29 12:13:16 +0100

    testing add tag

--- langs/rust/rustfix/src/msg_parse/mod.rs
+++ langs/rust/rustfix/src/msg_parse/mod.rs
@@ -1,88 +1,80 @@
 #[cfg(test)]
-pub mod test;
-
+mod test;
 
 use std::collections::BTreeMap;
 use std::collections::LinkedList;
 
+mod errors {
+    pub const TAG_INVALID_CHAR: &'static str = "Invalid char in tag.";
+}
 
-#[derive(Debug)]
-pub struct Parsed {
+
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum ParsingState {
+    StReadingTag,
+    StReadingValue,
+    StFinished,
+}
+
+impl Default for ParsingState {
+    fn default() -> ParsingState {
+        ParsingState::StReadingTag
+    }
+}
+
+
+#[derive(Debug, Default, Eq, PartialEq)]
+pub struct ParsingInfo {
     pub msg_map: BTreeMap<u32, String>,
     pub body_length: u32,
     pub check_sum: u32,
     pub num_tags: u32,
     pub orig_msg: String,
-    pub errors: LinkedList<(u32, String)>,
+    pub errors: LinkedList<(u32, String)>, //  position, description
     pub msg_length: u32,
+
+    state: ParsingState,
+    reading_tag: String, //  optimization
+    reading_value: String, //  optimization */
+    current_field_error: Option<(u32, &'static str)>,
 }
 
 
-impl Parsed {
-    fn add_char(mut self, ch: char) -> () {
-        let repl_ch = if ch == 1u8 as char {
-            '^'
-        } else {
-            ch
-        };
-        self.msg_length += 1;
-        // self.curr_state_length += 1;
-        self.orig_msg.push(repl_ch);
+impl ParsingInfo {
+    pub fn new() -> ParsingInfo {
+        ParsingInfo { ..Default::default() }
     }
-}
 
+    pub fn add_char(&mut self, ch: char) -> () {
+        self.msg_length += 1;
+        self.orig_msg.push(transform_ch(ch));
+        match self.state {
+            ParsingState::StReadingTag => self.add_char_reading_tag(ch),
+            ParsingState::StReadingValue => (),
+            ParsingState::StFinished => (),
+        }
+    }
 
-
-
-#[derive(Debug)]
-pub enum ParsingState {
-    StReadingTag {
-        tag: String,
-    },
-    StReadingValue {
-        tag: String,
-        value: String,
-    },
-    StFinished,
-}
-
-impl ParsingState {
-    pub fn add_char(self, ch: char) -> ParsingState {
-        match self {
-            ParsingState::StReadingTag { mut tag } => {
-                ParsingState::StReadingTag {
-                    tag: {
-                        tag.push(ch);
-                        tag
-                    },
+    fn add_char_reading_tag(&mut self, ch: char) {
+        self.reading_tag.push(ch);
+        match self.current_field_error {
+            Some(_) => (),
+            None => {
+                if ch < '0' || ch > '9' {
+                    self.current_field_error = Some((self.msg_length,
+                                                     self::errors::TAG_INVALID_CHAR));
                 }
             }
-            ParsingState::StReadingValue { tag, mut value } => {
-                ParsingState::StReadingValue {
-                    tag: tag,
-                    value: {
-                        value.push(ch);
-                        value
-                    },
-                }
-            }
-            ParsingState::StFinished => ParsingState::StReadingTag { tag: String::new() },
         }
     }
 }
 
 
-
-pub struct ParsingInfo {
-    parsing_state: ParsingState,
-    parsed: Parsed,
-}
-
-
-
-impl ParsingInfo {
-    pub fn add_char(self, ch: char) -> () {
-        self.parsed.add_char(ch);
-        self.parsing_state.add_char(ch);
+fn transform_ch(ch: char) -> char {
+    if ch == 1u8 as char {
+        '^'
+    } else {
+        ch
     }
 }
--- langs/rust/rustfix/src/msg_parse/test.rs
+++ langs/rust/rustfix/src/msg_parse/test.rs
@@ -1,19 +1,134 @@
-// use msg_parse::Parsing;
-// use test_diff;
+use msg_parse::{ParsingInfo, errors};
+use test_diff;
 
 
-// #[test]
-// fn run_tests() {
-//     let mut finish = Parsing::default();
-//     finish.add_char('\u{1}');
+#[test]
+fn init_add_char() {
+    let mut parsing = ParsingInfo::new();
+    parsing.add_char('1');
 
-//     let check = Parsing {
-//         orig_msg: "^".to_string(),
-//         msg_length: 1,
-//         curr_state_length: 1,
-//         curr_string: "\u{1}".to_string(),
-//         ..Parsing::default()
-//     };
+    let check = ParsingInfo {
+        orig_msg: "1".to_string(),
+        msg_length: 1,
+        reading_tag: "1".to_string(),
+        ..Default::default()
+    };
 
-//     assert_eq_dif!(finish, check);
-// }
+    assert_eq_dif!(parsing, check);
+}
+
+
+#[test]
+fn init_add_chars() {
+    let mut parsing = ParsingInfo::new();
+    parsing.add_char('1');
+    parsing.add_char('2');
+    parsing.add_char('3');
+    parsing.add_char('4');
+    parsing.add_char('5');
+
+    let check = ParsingInfo {
+        orig_msg: "12345".to_string(),
+        msg_length: 5,
+        reading_tag: "12345".to_string(),
+        ..Default::default()
+    };
+
+    assert_eq_dif!(parsing, check);
+}
+
+#[test]
+fn invalid_first_char() {
+    let mut parsing = ParsingInfo::new();
+    parsing.add_char('a');
+
+    let check = ParsingInfo {
+        orig_msg: "a".to_string(),
+        msg_length: 1,
+        reading_tag: "a".to_string(),
+        current_field_error: Some((1, errors::TAG_INVALID_CHAR)),
+        ..Default::default()
+    };
+
+    assert_eq_dif!(parsing, check);
+}
+
+#[test]
+fn invalid_second_char() {
+    let mut parsing = ParsingInfo::new();
+    parsing.add_char('1');
+    parsing.add_char('a');
+
+    let check = ParsingInfo {
+        orig_msg: "1a".to_string(),
+        msg_length: 2,
+        reading_tag: "1a".to_string(),
+        current_field_error: Some((2, errors::TAG_INVALID_CHAR)),
+        ..Default::default()
+    };
+
+    assert_eq_dif!(parsing, check);
+}
+
+
+#[test]
+fn invalid_chars_2errors() {
+    let mut parsing = ParsingInfo::new();
+    parsing.add_char('1');
+    parsing.add_char('a');
+    parsing.add_char('b');
+
+    let check = ParsingInfo {
+        orig_msg: "1ab".to_string(),
+        msg_length: 3,
+        reading_tag: "1ab".to_string(),
+        current_field_error: Some((2, errors::TAG_INVALID_CHAR)),
+        ..Default::default()
+    };
+
+    assert_eq_dif!(parsing, check);
+}
+
+#[test]
+fn invalid_chars_2errors_andvalids() {
+    let mut parsing = ParsingInfo::new();
+    parsing.add_char('1');
+    parsing.add_char('2');
+    parsing.add_char('a');
+    parsing.add_char('b');
+    parsing.add_char('3');
+    parsing.add_char('4');
+
+    let check = ParsingInfo {
+        orig_msg: "12ab34".to_string(),
+        msg_length: 6,
+        reading_tag: "12ab34".to_string(),
+        current_field_error: Some((3, errors::TAG_INVALID_CHAR)),
+        ..Default::default()
+    };
+
+    assert_eq_dif!(parsing, check);
+}
+
+
+#[test]
+fn invalid_chars_2errors_and_valids_non_consecutives() {
+    let mut parsing = ParsingInfo::new();
+    parsing.add_char('1');
+    parsing.add_char('2');
+    parsing.add_char('a');
+    parsing.add_char('3');
+    parsing.add_char('b');
+    parsing.add_char('4');
+    parsing.add_char('5');
+
+    let check = ParsingInfo {
+        orig_msg: "12a3b45".to_string(),
+        msg_length: 7,
+        reading_tag: "12a3b45".to_string(),
+        current_field_error: Some((3, errors::TAG_INVALID_CHAR)),
+        ..Default::default()
+    };
+
+    assert_eq_dif!(parsing, check);
+}
