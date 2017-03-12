49f4658a215ed444a2bfebf522c323b4b59ea9bb
maiquel <jleahred@gmail.com>
2017-03-12 23:51:44 +0100

    lang haskell redefined jumla

--- langs/haskell/sambamba/src/Lib.hs
+++ langs/haskell/sambamba/src/Lib.hs
@@ -21,7 +21,9 @@ ijayo  i  =  () : i
 moja :: Idadi
 moja = ijayo sufuri
 
-
+uliopita :: Idadi -> Idadi
+uliopita (():i) = i
 
 jumla :: Idadi -> Idadi -> Idadi
-jumla i1 i2 = i1 ++ i2
+jumla i1 sufuri = i1
+jumla i1 i2 = jumla (ijayo i1) (uliopita i2)
