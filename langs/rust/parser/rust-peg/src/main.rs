547af40fbe581c08e0c736fe8094e6a3ed0d45ba
maiquel <jleahred@gmail.com>
2017-04-06 23:20:22 +0200

    lang rust parser adding product

--- langs/rust/parser/rust-peg/src/main.rs
+++ langs/rust/parser/rust-peg/src/main.rs
@@ -28,8 +28,19 @@ mod calc {
         assert!(expr("1+2") == Ok(3.0));
         assert!(expr("2-5") == Ok(-3.0));
         assert!(expr("1+2+3") == Ok(6.0));
+        assert!(expr("-1+2+3-5") == Ok(-1.0));
     }
 
+    #[test]
+    fn prod() {
+        assert!(expr("+1*8") == Ok(8.0));
+        assert!(expr("-2.0*0.5") == Ok(-1.0));
+        assert!(expr("5*2") == Ok(10.0));
+        assert!(expr("1+2*3") == Ok(7.0));
+        println!("{:?}", expr("4*2*3-5"));
+        assert!(expr("4*2*3-5") == Ok(19.0));
+        assert!(expr("-4*2-3*5") == Ok(-23.0));
+    }
 
 }
 
