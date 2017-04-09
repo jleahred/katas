1ef1b1de0d8f315c0c9a1c281aa153a86498f788
maiquel <jleahred@gmail.com>
2017-04-09 18:51:17 +0200

    langs rust dtc Dynamic Text Compiler first commit

---
+++ langs/rust/dtc/src/lib.rs
@@ -0,0 +1,9 @@
+mod dtc {
+    include!(concat!(env!("OUT_DIR"), "/dtc.rs"));
+
+    // #[test]
+    // fn simple_numbers() {
+    //     assert!(expr("1.0") == Ok(1.0));
+    //     assert!(expr("0.1") == Ok(0.1));
+    // }
+}
