f14f2665feb01dfbf51c3369a7fa839cb0291ca4
maiquel <jleahred@gmail.com>
2017-07-01 20:26:08 +0200

    added c# multikata mini lists

--- multikata/square_list_equal/rust/src/main.rs
+++ multikata/square_list_equal/rust/src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
     sol1();
     sol2();
-    sol3();
 }
 
 
@@ -47,39 +46,6 @@ fn sol2() {
 fn comp(mut a: Vec<i32>, mut b: Vec<i32>) -> bool {
     for mut e in &mut a {
         *e = *e * *e;
-    }
-    a.sort();
-    b.sort();
-    a == b
-}
-
-
-fn sol3() {
-    let a = vec![3, 1, 2];
-    let b = vec![1, 4, 9];
-
-    println!("{}", comp3(a, b));
-}
-
-
-trait Sort {
-    type Item;
-    fn sort(self) -> Self;
-}
-impl Sort for Vec<i32> {
-    type Item = i32;
-    fn sort(self) -> Self {
-        self.sort();
-        self
-    }
-}
-
-fn comp3<C>(mut a: C, mut b: C) -> bool
-    where C: Eq + IntoIterator + Iterator + Sort,
-          C::Item: std::ops::Mul
-{
-    for mut e in &mut a {
-        e = e * e;
     }
     a.sort();
     b.sort();
