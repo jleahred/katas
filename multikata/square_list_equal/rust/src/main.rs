17f46cad63d6d4b5da162de952fc53ddf0566bbd
maiquel <jleahred@gmail.com>
2017-07-08 20:46:36 +0200

    added rust O(n) alg comp sets

--- multikata/square_list_equal/rust/src/main.rs
+++ multikata/square_list_equal/rust/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     sol1();
     sol2();
+    sol3();
 }
 
 
@@ -50,4 +51,40 @@ fn comp(mut a: Vec<i32>, mut b: Vec<i32>) -> bool {
     a.sort();
     b.sort();
     a == b
+}
+
+
+
+fn sol3() {
+    println!("{}", comp_hm(&[3, 1, 2], &[1, 4, 9]));
+    println!("{}", comp_hm(&[3, 1, 2, 1, 2], &[1, 4, 9, 4, 1]));
+    println!("{}", comp_hm(&[3, 1, 2], &[1, 9]));
+    println!("{}", comp_hm(&[1, 1, 1, 1], &[4, 0, 0, 0]));
+}
+
+
+
+
+use std::collections::HashMap;
+
+fn comp_hm(a: &[i32], b: &[i32]) -> bool {
+    let mut ha = HashMap::new();
+    let mut hb = HashMap::new();
+
+    let mut sa = vec![];
+    for e in a {
+        sa.push(e * e);
+    }
+
+    let fill_hm = |c, hm: &mut HashMap<i32, i32>| {
+        for &e in c {
+            let count = hm.entry(e).or_insert(0);
+            *count += 1;
+        }
+    };
+
+    fill_hm(b, &mut hb);
+    fill_hm(&sa, &mut ha);
+
+    ha == hb
 }
