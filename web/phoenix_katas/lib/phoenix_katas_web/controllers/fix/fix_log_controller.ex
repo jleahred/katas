101050fcace8f66d4d4a7482dd78965943892e8b
jleahred <jleahred@gmail.com>
2019-04-15 01:16:51 +0200

    phoenix working on fix log form

--- web/phoenix_katas/lib/phoenix_katas_web/controllers/fix/fix_log_controller.ex
+++ web/phoenix_katas/lib/phoenix_katas_web/controllers/fix/fix_log_controller.ex
@@ -5,8 +5,47 @@ defmodule PhoenixKatasWeb.FixLogController do
   # %{"any" => "aa", "connection" => "Any", "date" => "2019-04-14", "dir" => "Both", "msg_type" => "Any"}
   @spec fix_log(Plug.Conn.t(), atom() | binary() | keyword() | map()) :: Plug.Conn.t()
   def fix_log(conn, par) do
-    render(conn, "fix_log.html", par)
+    render(conn, "fix_log.html", Map.merge(def_par(), par2atomkey(par)))
+    # text(conn, "#{inspect(Map.merge(def_par(), par2atomkey(par)))}")
+    # text(conn, "#{inspect(par2atomkey(par))}")
+    # text(conn, "#{inspect(def_par())}")
+    # render(conn, "fix_log.html", date: today_string())
+    # text(conn, "#{d.year}-#{d.month}-#{d.day}")
     # text(conn, "#{inspect(par)}")
+    # text(conn, "#{inspect(string_map2atom_map(par))}")
     # text(conn, "here")
+  end
+
+  @spec today_string() :: <<_::16, _::_*8>>
+  defp today_string() do
+    d = DateTime.utc_now()
+    year = d.year |> Integer.to_string() |> String.pad_leading(4, "0")
+    month = d.month |> Integer.to_string() |> String.pad_leading(2, "0")
+    day = d.day |> Integer.to_string() |> String.pad_leading(2, "0")
+    "#{year}-#{month}-#{day}"
+  end
+
+  defp def_par() do
+    %{
+      any: "",
+      connection: "any",
+      date: today_string(),
+      dir: "both",
+      msg_type: "any"
+    }
+  end
+
+  defp par2atomkey(smap) do
+    sdefpar =
+      def_par()
+      |> Enum.map(fn {k, v} ->
+        {Atom.to_string(k), v}
+      end)
+      |> Enum.into(%{})
+
+    smap
+    |> Enum.filter(&Map.has_key?(sdefpar, elem(&1, 0)))
+    |> Enum.map(fn {k, v} -> {String.to_atom(k), v} end)
+    |> Enum.into(%{})
   end
 end
