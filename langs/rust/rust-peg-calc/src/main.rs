c110cb36feba100af41509623a29d6fb4539c251
jleahred <jleahred@gmail.com>
2019-09-16 23:00:55 +0200

    rust peg-calc starting parser to program

--- langs/rust/rust-peg-calc/src/main.rs
+++ langs/rust/rust-peg-calc/src/main.rs
@@ -3,12 +3,12 @@ use peg::parser;
 
 #[derive(Clone, Copy)]
 enum Command {
-    Push(f64),
-    Oper(Operation),
+    PushVal(f64),
+    Fun(Function),
 }
 
 #[derive(Clone, Copy)]
-enum Operation {
+enum Function {
     Add,
     Subs,
     Mult,
@@ -18,9 +18,9 @@ enum Operation {
 type Stack = Vec<f64>;
 
 #[derive(Default)]
-struct Program {
+pub struct Program {
     commands: Vec<Command>,
-    stack: Stack,
+    // stack: Stack,
 }
 
 impl Program {
@@ -30,38 +30,46 @@ impl Program {
 }
 
 impl Program {
-    fn add_command(mut self, command: Command) -> Program {
+    fn add_cmd(mut self, command: Command) -> Program {
         self.commands.push(command);
         self
     }
-    fn execute(self) -> Result<f64, String> {
+
+    fn add_prog(mut self, mut program: Program) -> Self {
+        self.commands.append(&mut program.commands);
+        self
+    }
+
+    fn execute(&self) -> Result<f64, String> {
         let final_stack = self
             .commands
             .iter()
-            .try_fold(self.stack, |stack, c| exec_command(*c, stack))?;
+            .try_fold(vec![], |stack, c| exec_command(*c, stack))?;
 
         if final_stack.len() != 1 {
-            Err("end of program with too many elements on stack".to_string())
+            Err("Incorrect program!!! end of program with too many elements on stack".to_string())
         } else {
-            Ok(*final_stack.last().ok_or("final program with empty stack")?)
+            Ok(*final_stack
+                .last()
+                .ok_or("Incorrect program!!! final program with empty stack")?)
         }
     }
 }
 
 fn exec_command(command: Command, mut stack: Stack) -> Result<Stack, String> {
     Ok(match command {
-        Command::Push(value) => stack.ipush(value),
-        Command::Oper(op) => {
+        Command::PushVal(value) => stack.ipush(value),
+        Command::Fun(op) => {
             let (f, s) = match (stack.pop(), stack.pop()) {
                 (None, _) => Err("exec command with empty stack"),
                 (_, None) => Err("exec command with empty stack"),
                 (Some(f), Some(s)) => Ok((f, s)),
             }?;
             let r = match op {
-                Operation::Add => s + f,
-                Operation::Subs => s - f,
-                Operation::Mult => s * f,
-                Operation::Div => s / f,
+                Function::Add => s + f,
+                Function::Subs => s - f,
+                Function::Mult => s * f,
+                Function::Div => s / f,
             };
 
             stack.ipush(r)
@@ -69,6 +77,7 @@ fn exec_command(command: Command, mut stack: Stack) -> Result<Stack, String> {
     })
 }
 
+#[cfg(test)]
 fn cmp_float(l: f64, r: f64) -> Option<std::cmp::Ordering> {
     if (l, r) == (0.0, 0.0) {
         Some(std::cmp::Ordering::Equal)
@@ -79,6 +88,7 @@ fn cmp_float(l: f64, r: f64) -> Option<std::cmp::Ordering> {
     }
 }
 
+#[cfg(test)]
 fn eq_float(l: f64, r: f64) -> bool {
     match cmp_float(l, r) {
         Some(std::cmp::Ordering::Equal) => true,
@@ -89,30 +99,60 @@ fn eq_float(l: f64, r: f64) -> bool {
 #[test]
 pub fn test_machine() {
     let program = Program::new()
-        .add_command(Command::Push(1.0))
-        .add_command(Command::Push(4.0))
-        .add_command(Command::Oper(Operation::Add));
+        .add_cmd(Command::PushVal(1.0))
+        .add_cmd(Command::PushVal(4.0))
+        .add_cmd(Command::Fun(Function::Add));
     assert!(eq_float(program.execute().unwrap(), 5.0f64));
 
     let program = Program::new()
-        .add_command(Command::Push(1.0))
-        .add_command(Command::Push(2.0))
-        .add_command(Command::Push(3.0))
-        .add_command(Command::Oper(Operation::Mult))
-        .add_command(Command::Oper(Operation::Add));
+        .add_cmd(Command::PushVal(1.0))
+        .add_cmd(Command::PushVal(2.0))
+        .add_cmd(Command::PushVal(3.0))
+        .add_cmd(Command::Fun(Function::Mult))
+        .add_cmd(Command::Fun(Function::Add));
     assert!(eq_float(program.execute().unwrap(), 7.0f64));
 
     let program = Program::new()
-        .add_command(Command::Push(8.0))
-        .add_command(Command::Push(2.0))
-        .add_command(Command::Oper(Operation::Div));
+        .add_cmd(Command::PushVal(8.0))
+        .add_cmd(Command::PushVal(2.0))
+        .add_cmd(Command::Fun(Function::Div));
     assert!(eq_float(program.execute().unwrap(), 4.0f64));
+    assert!(eq_float(program.execute().unwrap(), 4.0f64));
+}
+
+fn prog_from_term(l: Program, r: Program, op: &str) -> Program {
+    let operation = match op {
+        "+" => Function::Add,
+        "-" => Function::Subs,
+        _ => unreachable!(),
+    };
+    Program::new()
+        .add_prog(l)
+        .add_prog(r)
+        .add_cmd(Command::Fun(operation))
+}
+
+fn prog_from_number(n: &str) -> Result<Program, &'static str> {
+    // match n.parse::<f64>() {
+    //     Ok(number) => Ok(Program::new().add_cmd(Command::PushVal(number))),
+    //     _ => Err("failed parsing number")
+    // }
+    n.parse()
+        .and_then(|number| Ok(Program::new().add_cmd(Command::PushVal(number))))
+        .or_else(|_| Err("failed parsing number"))
 }
 
 parser! {
     grammar math_parser() for str {
-        pub rule expr() -> f64
-            = number()
+        pub rule compile() -> Program
+            = expr()
+
+        rule expr() -> Program
+            =   term()
+
+        rule term() -> Program
+            =   l:number() spc()  op:$("+" / "-") spc() r:term()  {  prog_from_term(l, r, op) }
+            /   number()
         // pub rule expr() -> f64
         //     = _() r:(
         //           "+" _() "("  _()  e:expr()  _() ")"  _()  { e }
@@ -147,9 +187,8 @@ parser! {
 
 
         //  number      ------------------------
-        rule number() -> f64
-            = n:$(['0'..='9']+("." ['0'..='9']+)?)
-                {? n.parse().or_else(|_| Err("failed parsing number")) }
+        rule number() -> Program
+            = n:$(['0'..='9']+("." ['0'..='9']+)?)    {? prog_from_number(n) }
 
         //  spaces      ------------------------
         rule _()  = quiet!{[' ' | '\t' | '\n' | '\r']*}
@@ -161,14 +200,17 @@ pub fn main() {}
 
 #[test]
 pub fn test_1() {
-    assert_eq!(math_parser::expr("1"), Ok(1.0));
-    // assert_eq!(math_parser::expr("123"), Ok(123.0));
-    // assert_eq!(math_parser::expr("123.12"), Ok(123.12));
+    assert_eq!(math_parser::compile("1").unwrap().execute(), Ok(1.0));
+    assert_eq!(math_parser::compile("123").unwrap().execute(), Ok(123.0));
+    assert_eq!(
+        math_parser::compile("123.12").unwrap().execute(),
+        Ok(123.12)
+    );
     // assert_eq!(math_parser::expr("+123.12"), Ok(123.12));
     // assert_eq!(math_parser::expr("-123.12"), Ok(-123.12));
     // assert_eq!(math_parser::expr("- 123.12"), Ok(-123.12));
-    // assert_eq!(math_parser::expr("1+2"), Ok(3.0));
-    // assert_eq!(math_parser::expr("1-2"), Ok(-1.0));
+    assert_eq!(math_parser::compile("1+2").unwrap().execute(), Ok(3.0));
+    assert_eq!(math_parser::compile("1-2").unwrap().execute(), Ok(-1.0));
     // assert_eq!(math_parser::expr("+ 1 - 2"), Ok(-1.0));
     // assert_eq!(math_parser::expr("(1)"), Ok(1.0));
     // assert_eq!(math_parser::expr("((1))"), Ok(1.0));
