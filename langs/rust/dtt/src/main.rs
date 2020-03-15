11f371eb243d29c79827dd968e19ae43201fbdcf
jleahred <jleahred@gmail.com>
2020-03-15 09:59:52 +0100

    wip

--- langs/rust/dtt/src/main.rs
+++ langs/rust/dtt/src/main.rs
@@ -1,6 +1,20 @@
+
 extern crate dtt;
+
+//  generate rules to parse peg ;-)
+// extern crate dynparser;
+
+// fn main() {
+//     dtt::peg::peg2code::print_rules2parse_peg();
+// }
+
 extern crate dynparser;
 
 fn main() {
-    dtt::peg::peg2code::print_rules2parse_peg();
+    let peg = "
+    main    =   a+
+    a       =   name:'a'  
+    ";
+    let rules = dtt::peg::rules_from_peg(peg);
+    println!("{:#?}", rules);
 }
